73eaf1b5a004cb4579621db177edfea0a55c01d5
apple <geekspeng@icloud.com>
2015-05-06 22:52:23 +0800

    增加文件扫描

--- app/src/main/java/com/yzy/supercleanmaster/ui/VirusKillActivity.java
+++ app/src/main/java/com/yzy/supercleanmaster/ui/VirusKillActivity.java
@@ -411,14 +411,6 @@ public class VirusKillActivity extends BaseSwipeBackActivity {
         }
     }
 
-
-
-
-
-
-
-
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
